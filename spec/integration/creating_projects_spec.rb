9109bb7d1abf4ecac681c103b50c4d5aa2ed6cb0
Tyler Morgan <toasty@localhost.localdomain>
2013-05-08 17:27:42 -0700

    Ability to create a new project complete

--- spec/integration/creating_projects_spec.rb
+++ spec/integration/creating_projects_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'Creating Projects' do
 	scenario "can create a project" do
 		visit '/'
-		clink_link "New Project"
+		click_link "New Project"
 		fill_in 'Name', :with => 'TextMate 2'
 		fill_in 'Description', :with => "A text editor for OSX"
 		click_button 'Create Project'
