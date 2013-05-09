bf3169b2859ac79256d7b01db519e99feccff57e
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-09 16:50:04 -0700

    Added project destroy functionality

---
+++ spec/integration/deleting_projects_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+feature "deleting projects" do
+	scenario "Deleteing a project" do
+		Factory(:project, :name => "TextMate 2")
+		visit '/'
+		click_link "TextMate 2"
+		click_link "Delete Project"
+		page.should have_content("Project has been deleted.")
+
+		visit '/'
+		page.should_not have_content("TexteMate 2")
+	end
+end
