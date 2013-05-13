da1ded781ff39be1472f8d4073b818421acd366c
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-13 15:30:13 -0700

    Added a view for tickets

---
+++ spec/integration/viewing_tickets_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature "Viewing Tickets" do
+  before do
+    textmate_2 = Factory(:project, :name => "TextMate 2")
+    Factory(:ticket, :project => textmate_2,
+      :title => "Make it shiny!", :description => "Gradients, starbursts, oh my!" )
+    internet_exlorer = Factory(:project, :name => "Internet Explorer")
+    Factory(:ticket, :project => internet_exlorer,
+      :title => "Standards compliance", :description => "It isn't a joke")
+    visit '/'
+  end
+
+  scenario "Viewing tickets for a specified project" do
+    click_link "TextMate 2"
+    page.should have_content("Make it shiny!")
+    page.should_not have_content("Standards compliance")
+
+    click_link "Make it shiny!"
+    within("#ticket h2") do
+      page.should have_content("Make it shiny!")
+    end
+
+    page.should have_content("Gradients, starbursts, oh my!")
+  end
+end
---
+++ spec/support/factories/ticket_factory.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :ticket do
+    title "example ticket"
+    description "An example ticket, nothing more"
+  end
+end
