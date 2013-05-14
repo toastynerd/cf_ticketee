c60443cd4655b6b2224fac0d525e8608c4f79fff
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-14 15:01:41 -0700

    Added a feature for signing in via form

--- spec/integration/users/signing_in_spec.rb
+++ spec/integration/users/signing_in_spec.rb
@@ -11,4 +11,14 @@ feature "signing in" do
     page.should have_content("Your account was successfully confirmed")
     page.should have_content("Signed in as ticketee@example.com")
   end
+
+  scenario "Signing in through a form" do
+    User.find_by_email('ticketee@example.com').confirm!
+    visit '/'
+    click_link "Sign in"
+    fill_in 'Email', :with =>"ticketee@example.com"
+    fill_in 'Password', :with => "foobar"
+    click_button "Sign in"
+    page.should have_content("Signed in successfully")
+  end
 end
