1437f935be7e04afaafd14d054fe4e627eacb5f8
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-16 09:34:42 -0700

    added users management for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Ticketee::Application.routes.draw do
-  get "admin/users/index"
-
   devise_for :users
 
   root :to => "projects#index"
   resources :projects do
     resources :tickets
+  end
+  namespace :admin do
+    root :to => "base#index"
+    resources :users
   end
   # The priority is based upon order of creation:
   # first created -> highest priority.
