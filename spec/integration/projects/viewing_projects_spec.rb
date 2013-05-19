962c50beea6897275f1b01dc0aae0e625e85c762
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-19 12:06:18 -0700

    Don't show projects that the user doesn't have permission to see.

--- spec/integration/projects/viewing_projects_spec.rb
+++ spec/integration/projects/viewing_projects_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 feature "Viewing Projects" do
   let!(:user) { Factory(:confirmed_user)}
   let!(:project) {Factory(:project)}
+  let!(:project2) {Factory(:project, :name => "Internet Explorer")}
 
   before do
     sign_in_as!(user)
@@ -11,6 +12,7 @@ feature "Viewing Projects" do
 
   scenario "listing all projects" do
     visit '/'
+    page.should_not have_content("Internet Explorer")
     click_link project.name
     page.current_url.should == project_url(project)
   end
