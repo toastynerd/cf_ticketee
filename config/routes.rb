e158f0ad28999b2f6c873c97086a3a5912940fcb
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-19 18:46:57 -0700

    added admin permissions screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Ticketee::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   get '/awaiting_confirmation', :to => "users#confirmation", :as => "confirm_user"
+  put '/admin/users/:user_id/permissions', 
+      :to => 'admin/permissions#update',
+      :as => :update_user_permissions
+
 
   root :to => "projects#index"
   resources :projects do
     resources :tickets
   end
+
   namespace :admin do
     root :to => "base#index"
-    resources :users
+    resources :users do
+      resources :permissions
+    end
   end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/support/authorization_helpers.rb
+++ spec/support/authorization_helpers.rb
@@ -4,7 +4,12 @@ module AuthorizationHelpers
                       :action => action,
                       :thing => thing)
   end
+
+  def check_permission_box(permission, object)
+    check "permissions_#{object.id}_#{permission}"
+  end
 end
+
 
 RSpec.configure do |c|
   c.include AuthorizationHelpers
