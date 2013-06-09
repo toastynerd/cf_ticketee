48da8baf600b1bcdd40e7a629322b2770819aed4
tylermorgan86 <tylermorgan86@gmail.com>
2013-06-09 14:03:18 -0700

    changed default rails readme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
 
 private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+    {:locale => I18n.locale}
+  end
+
   def authorize_admin!
     authenticate_user!
     unless current_user.admin?
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Ticketee::Application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
-  get '/awaiting_confirmation', :to => "users#confirmation", :as => "confirm_user"
-  put '/admin/users/:user_id/permissions', 
-      :to => 'admin/permissions#update',
-      :as => :update_user_permissions
+  scope ":locale" do
+    devise_for :users, :controllers => {:registrations => "registrations"}
+    get '/awaiting_confirmation', :to => "users#confirmation", :as => "confirm_user"
+    put '/admin/users/:user_id/permissions', 
+        :to => 'admin/permissions#update',
+        :as => :update_user_permissions
 
 
-  root :to => "projects#index"
-  resources :projects do
-    resources :tickets
-  end
-
-  resources :files
-
-  namespace :admin do
-    root :to => "base#index"
-    resources :users do
-      resources :permissions
+    resources :projects do
+      resources :tickets
     end
-  end
+
+    resources :files
+
+    namespace :admin do
+      root :to => "base#index"
+      resources :users do
+        resources :permissions
+      end
+    end
+
+  end  
+  root :to => "projects#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
